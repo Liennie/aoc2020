7f6f3dcbc9eda443defcdf79eb490618a10cb2b2
Trbola, Martin <martin.trbola@firma.seznam.cz>
2020-12-08 11:22:57 +0100

    Day 8 fixed number of arguments

--- common/asm/instruction.go
+++ common/asm/instruction.go
@@ -6,39 +6,46 @@ import (
 	"github.com/liennie/aoc2020/common/asm/op"
 )
 
+const (
+	argc = 1
+)
+
 type Instruction struct {
 	Line int
 	Op   op.Code
-	Args []int
+	Args [argc]int
 }
 
 func newInstruction(line int, o op.Code, args ...int) (*Instruction, error) {
-	if len(args) != 1 {
-		return nil, fmt.Errorf("Instruction %s needs 1 arg", o)
+	if len(args) != argc {
+		return nil, fmt.Errorf("Instruction %s needs %d args", o, argc)
 	}
 
-	return &Instruction{
+	i := &Instruction{
 		Line: line,
 		Op:   o,
-		Args: args,
-	}, nil
+	}
+
+	copy(i.Args[:], args[:argc])
+
+	return i, nil
 }
 
 func (i Instruction) Exec(p *Program) {
-	exec[i.Op](p, i.Args...)
+	exec[i.Op](p, i.Args)
 }
 
-var exec = map[op.Code]func(p *Program, args ...int){
+var exec = map[op.Code]func(p *Program, args [argc]int){
 	// Nop
-	op.Nop: func(p *Program, args ...int) {},
+	op.Nop: func(p *Program, args [argc]int) {},
 
 	// Acc
-	op.Acc: func(p *Program, args ...int) {
+	op.Acc: func(p *Program, args [argc]int) {
 		p.reg.Acc += args[0]
 	},
 
 	// Jmp
-	op.Jmp: func(p *Program, args ...int) {
+	op.Jmp: func(p *Program, args [argc]int) {
 		p.reg.Ip += args[0] - 1
 	},
 }
