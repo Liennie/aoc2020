69f56eb9eeeb80cfa008cc23a61a41068a4d62cb
Trbola, Martin <martin.trbola@firma.seznam.cz>
2020-12-15 11:12:30 +0100

    Day 15 part 2

--- 15/15.go
+++ 15/15.go
@@ -18,22 +18,35 @@ func parse(filename string) []int {
 	return res
 }
 
+func solve(numbers []int, m int) int {
+	i := 0
+
+	prev := map[int]int{}
+	for ; i < len(numbers)-1; i++ {
+		prev[numbers[i]] = i
+	}
+
+	cur := numbers[i]
+
+	for ; i < m-1; i++ {
+		if p, ok := prev[cur]; ok {
+			prev[cur], cur = i, i-p
+		} else {
+			prev[cur], cur = i, 0
+		}
+	}
+	return cur
+}
+
 func main() {
 	defer util.Recover(log.Err)
 
 	numbers := parse(input)
 
 	// Part 1
-	for i := len(numbers) - 1; i < 2019; i++ {
-		n := numbers[i]
-		next := 0
-		for j := i - 1; j >= 0; j-- {
-			if numbers[j] == n {
-				next = i - j
-				break
-			}
-		}
-		numbers = append(numbers, next)
-	}
-	log.Part1(numbers[2019])
+	log.Part1(solve(numbers, 2020))
+
+	// Part 2
+	log.Part2(solve(numbers, 30000000))
+
 }
