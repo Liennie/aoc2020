c09c83949d5cc3de6c3095ec76a17254e40894b0
Trbola, Martin <martin.trbola@firma.seznam.cz>
2020-12-23 18:54:49 +0100

    Day 23 part 2

--- 23/23.go
+++ 23/23.go
@@ -1,78 +1,101 @@
 package main
 
 import (
+	"container/ring"
+
 	"github.com/liennie/aoc2020/common/load"
 	"github.com/liennie/aoc2020/common/log"
 	"github.com/liennie/aoc2020/common/util"
 )
 
 type cups struct {
-	cups []int
-	cur  int
-	max  int
-	len  int
+	cur *ring.Ring
+	dir map[int]*ring.Ring
+	max int
+	len int
 }
 
-func (c cups) clone() cups {
-	res := cups{
-		cups: make([]int, len(c.cups)),
-		cur:  c.cur,
-		max:  c.max,
-		len:  c.len,
+func (c *cups) clone() *cups {
+	res := &cups{
+		cur: ring.New(c.len),
+		dir: map[int]*ring.Ring{},
+		max: c.max,
+		len: c.len,
 	}
-	copy(res.cups, c.cups)
+	c.cur.Do(func(v interface{}) {
+		res.cur.Value = v
+		res.dir[v.(int)] = res.cur
+		res.cur = res.cur.Next()
+	})
 	return res
 }
 
-func (c *cups) move() {
-	rem := [3]int{}
-	for i := range rem {
-		rem[i] = c.cups[(c.cur+i+1)%c.len]
+func (c *cups) expand(to int) *cups {
+	x := ring.New(to - c.len)
+	for i := 0; i < to-c.len; i++ {
+		x.Value = c.max + i + 1
+		c.dir[c.max+i+1] = x
+		x = x.Next()
 	}
+	c.cur.Prev().Link(x)
+	c.max += to - c.len
+	c.len = to
+	return c
+}
 
-	dst := 0
-	min := c.max
-	for i := 4; i < c.len; i++ {
-		d := (c.cur + i) % c.len
-		m := util.Mod(c.cups[c.cur]-c.cups[d], c.max)
-		if m < min {
-			dst = d
-			min = m
+func (c *cups) move() {
+	rem := c.cur.Unlink(3)
+
+	ex := map[int]bool{}
+	rem.Do(func(v interface{}) {
+		ex[v.(int)] = true
+	})
+
+	var dst *ring.Ring
+	m := c.cur.Value.(int) - 1
+	for i := 1; i < c.max; i++ {
+		if !ex[m] {
+			if d, ok := c.dir[m]; ok {
+				dst = d
+				break
+			}
+		}
+		m--
+		if m <= 0 {
+			m = c.max
 		}
 	}
 
-	cnt := util.Mod(dst-(c.cur+3), c.len)
-	for i := 0; i < cnt; i++ {
-		c.cups[(c.cur+i+1)%c.len] = c.cups[(c.cur+i+4)%c.len]
+	if dst == nil {
+		util.Panic("Destination cup not found")
 	}
 
-	for i := range rem {
-		c.cups[(c.cur+cnt+i+1)%c.len] = rem[i]
-	}
+	dst.Link(rem)
+	c.cur = c.cur.Next()
+}
 
-	c.cur++
-	c.cur %= c.len
+func (c *cups) one() *ring.Ring { // to rule them all
+	if one, ok := c.dir[1]; ok {
+		return one
+	}
+	util.Panic("1 not found")
+	return nil
 }
 
 func (c *cups) order() int {
-	s := 0
-	for ; s < c.len; s++ {
-		if c.cups[s] == 1 {
-			break
-		}
-	}
-
+	r := c.one().Next()
 	o := 0
 	for i := 1; i < c.len; i++ {
 		o *= 10
-		o += c.cups[(s+i)%c.len]
+		o += r.Value.(int)
+		r = r.Next()
 	}
-
 	return o
 }
 
-func parse(filename string) cups {
-	res := cups{}
+func parse(filename string) *cups {
+	res := &cups{}
+	ns := []int{}
 	for l := range load.File(filename) {
 		c := util.Atoi(l)
 		if c > res.max {
@@ -81,9 +104,16 @@ func parse(filename string) cups {
 		if c <= 0 {
 			util.Panic("No")
 		}
-		res.cups = append(res.cups, c)
+		ns = append(ns, c)
 	}
-	res.len = len(res.cups)
+	res.len = len(ns)
+	res.dir = map[int]*ring.Ring{}
+	res.cur = ring.New(res.len)
+	for i := 0; i < res.len; i++ {
+		res.cur.Value = ns[i]
+		res.dir[ns[i]] = res.cur
+		res.cur = res.cur.Next()
+	}
 	return res
 }
 
@@ -98,4 +128,12 @@ func main() {
 		c.move()
 	}
 	log.Part1(c.order())
+
+	// Part 2
+	c = oc.clone().expand(1000000)
+	for i := 0; i < 10000000; i++ {
+		c.move()
+	}
+	one := c.one()
+	log.Part2(one.Next().Value.(int) * one.Next().Next().Value.(int))
 }
